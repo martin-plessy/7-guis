cd053c9f61c8d894472a39357d66c7e332244721
Martin Plessy <plessy.martin@outlook.com>
2021-10-23 17:13:32 +0100

    Vue JS 3.x with Vite and TypeScript

---
+++ 1.Counter/VueJs/src/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+  <input type="number" readonly v-bind:valueAsNumber="counterValue" data-test-id="counterNumber"/>
+  <input type="button" autofocus value="Count" v-on:click="++counterValue" data-test-id="counterButton"/>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    counterValue: 0
+  })
+}
+</script>
